14f5d398d6300d43b8b2c11c7c42ee0789a04e74
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2025-12-21 15:15:44 +0700

    Add files via upload

--- blogger.editor.js
+++ blogger.editor.js
@@ -1,3 +1,14 @@
+const blogger_post_title_class = 'h3.post-title';
+const blogger_post_comment_header = 'Post comment to Quan Bua';
+
+const comment_page_head_divid = 'comment_page_head';
+const comment_page_name_divid = 'comment_page_name';
+const comment_container_divid = 'comment_container';
+
+const urlBloggerCommentIframe = 'https://www.blogger.com/comment/frame';
+const urlPostCommentMinPage = 'https://cdn.jsdelivr.net/gh/antonroch/blogger/blogger.comment.min.html';
+const urlPostCommentPage = 'https://cdn.jsdelivr.net/gh/antonroch/blogger/blogger.comment.html';
+
 String.prototype.escapebreaks = function(){return(this.replace(/<\/?p>/gi,'\n').replace(/<br\s*\/?>/gi,'\n').replace(/(\r?\n|\r){3,}/g,'\n\n'))}
 
 function editorInit(config={dialogsInBody:true}, note='#summernote'){
@@ -24,11 +35,15 @@ function getBloggerPostID(){
 }
 
 function getBloggerPostCommentURL(){
-  return `https://www.blogger.com/comment/frame/${getBloggerBlogID()}?po=${getBloggerPostID()}`;
+  return `${urlBloggerCommentIframe}/${getBloggerBlogID()}?po=${getBloggerPostID()}`;
 }
 
 function openBloggerPostCommentPage(){
   window.open(getBloggerPostCommentURL(), '_blank').focus();
+}
+
+function openBloggerPostCommentFrame(){
+  window.open(`${urlPostCommentMinPage}?blog=${getBloggerBlogID()}&post=${getBloggerPostID()}&head=${blogger_post_comment_header}&name=${editorGetPostTitle()}`).focus();
 }
 
 // Using global html2cmt() and copytext(text)
@@ -36,7 +51,7 @@ function editorParseCode(nobreaks=true, note='#summernote'){
   var commentCode = editorGetCode(note);
   comment = commentCode.html2cmt();
   copytext(comment.trim());
-  openBloggerPostCommentPage();
+  openBloggerPostCommentFrame();
 }
 
 function editorSetCode(code='', note='#summernote'){
@@ -47,6 +62,10 @@ function editorSetFocus(note='.note-editable'){
   $(note).trigger('focus');
 }
 
+function editorGetPostTitle(title_class=blogger_post_title_class){
+  return document.querySelector(title_class).textContent;
+}
+
 function checkImage(url){
   return new Promise((resolve)=>{
     const img = new Image();
@@ -54,4 +73,16 @@ function checkImage(url){
     img.onerror=()=>{resolve(false)};
     img.src = url;
   });
+}
+
+function extractUrlQuery(query_string){
+  let p = new URLSearchParams(query_string.split('?')[1]);
+  return p.size==0 ? {} : Object.fromEntries(p.entries());
+}
+
+function editorSetCommentPage(src=null, blog='blog', post='post', head='head', name='name'){
+  let dat = extractUrlQuery(window.location.href);
+  if(dat[head]) document.getElementById(comment_page_head_divid).innerHTML = dat[head];
+  if(dat[name]) document.getElementById(comment_page_name_divid).innerHTML = dat[name];
+  document.getElementById(comment_container_divid).src = src || `${urlBloggerCommentIframe}/${dat[blog]}?po=${dat[post]}`;
 }
